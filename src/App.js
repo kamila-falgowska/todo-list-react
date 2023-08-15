db78e5031965ee9e8a9d977ca2c4e8ce53c078c1
Kamila Falgowska <kamilafalgowska@gmail.com>
2023-08-15 19:58:29 +0200

    task list without interactions yet

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Header from "./Header";
 import Container from "./Container";
 
 const tasks = [
-  { id: 1, content: "przejść na Reacta", done: false },
-  { id: 2, content: "zjeść obiad", done: true },
+  { id: 1, content: "pójść na basen", done: false },
+  { id: 2, content: "zrobić śniadanie", done: true },
 ];
 
 const hideDoneTasks = false;
