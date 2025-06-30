bc23c609940e3cabeb0caea6cda5ea2fdbcc45f3
Kamila Falgowska <kamilafalgowska@gmail.com>
2025-06-30 18:10:31 +0200

    set all done interaction

--- src/App.js
+++ src/App.js
@@ -31,6 +31,10 @@ function App() {
     }));
   }
 
+  const setAllDone = () => {
+    setTasks(tasks => tasks.map(task => ({...task, done: true})));
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -54,6 +58,7 @@ function App() {
             tasks={tasks}
             hideDone={hideDone}
             toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
           />
         }
       />
