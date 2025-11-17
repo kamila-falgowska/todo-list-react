8fcc8948ce1c3fa8a71566d15632803cd4c2ee35
Kamila Falgowska <kamilafalgowska@gmail.com>
2025-11-17 19:28:44 +0100

    Refactor tasks logic into custom useTasks hook

---
+++ src/useTasks.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+
+const getInitialTasks = () => {
+    const tasksFromLocalStorage = localStorage.getItem("tasks");
+    return tasksFromLocalStorage ? JSON.parse(tasksFromLocalStorage) : [];
+};
+
+export const useTasks = () => {
+    const [tasks, setTasks] = useState(getInitialTasks);
+
+    useEffect(() => {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }, [tasks]);
+
+    const addTask = (content) => {
+        setTasks(tasks => [
+            ...tasks,
+            {
+                content,
+                done: false,
+                id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+            },
+        ]);
+    };
+
+    const removeTask = (id) => {
+        setTasks(tasks => tasks.filter(task => task.id !== id));
+    };
+
+    const toggleTaskDone = (id) => {
+        setTasks(tasks =>
+            tasks.map(task =>
+                task.id === id ? { ...task, done: !task.done } : task
+            )
+        );
+    };
+
+    const setAllDone = () => {
+        setTasks(tasks => tasks.map(task => ({ ...task, done: true })));
+    };
+
+    return {
+        tasks,
+        addTask,
+        removeTask,
+        toggleTaskDone,
+        setAllDone,
+    };
+};
