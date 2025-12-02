b809ba4e0249949a36eadf5f18e6f9fc2d05ea5a
Kamila Falgowska <kamilafalgowska@gmail.com>
2025-12-02 15:39:40 +0100

    Refactor AuthorPage structure

--- src/features/author/AuthorPage/index.js
+++ src/features/author/AuthorPage/index.js
@@ -1,6 +1,6 @@
-import { Container } from "../../common/Container/styled";
-import Section from "../../common/Section";
-import Header from "../../common/Header";
+import { Container } from "../../../common/Container/styled";
+import Section from "../../../common/Section";
+import Header from "../../../common/Header";
 
 const AuthorPage = () => (
     <Container>
